4b54b0e738f725f8a9761fbfa0745f3fda059f89
Mauricio Cruz <mauriciogonzalolopezcruz@gmail.com>
2022-05-15 20:05:46 -0300

    todas las validaciones funcionan

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <form action="informacion.php" method="post">
         ID: <input type="text" name="id">
@@ -13,8 +15,9 @@
         Apellido: <input type="text" name="apellido">
         Telefono: <input type="text" name="telefono">
         Direccion: <input type="text" name="direccion">
-
+        <input type="submit" name="enviar" value="Enviar">
 
     </form>
 </body>
+
 </html>
