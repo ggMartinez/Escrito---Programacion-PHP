e8e5655078f17d87cc87c8f9c80fd369ffd9e75c
veoDoble <gustavoguerrerom@gmail.com>
2022-05-16 03:14:42 -0300

    validación nombre y apellido

--- validaciones.php
+++ validaciones.php
@@ -9,18 +9,26 @@
 
     function validarNombre($nombre){
         // Validar que el nombre recibido tenga menos de 20 caracteres
+        if (isset($nombre) && 
+            strlen($nombre)<20) 
+            return true;
     }
 
     function validarApellido($apellido){
         // Validar que el apellido recibido tenga menos de 20 caracteres
+        if (isset($apellido) && 
+            strlen($apellido)<20) 
+            return true;
     }
 
     function validarTelefono($telefono){
         // Validar que el telefono recibido sea de 9 cifras
+        return true;
     }
 
     function validarDireccion($direccion){
         // Validar que la direccion recibida tenga menos de 50 caracteres
+        return true;
     }
 
     
