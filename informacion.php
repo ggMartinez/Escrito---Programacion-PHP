e75ef00470aa84e164398252a044079abd0b0731
veoDoble <gustavoguerrerom@gmail.com>
2022-05-16 11:16:46 -0300

    elimino comentarios superfluos

--- informacion.php
+++ informacion.php
@@ -2,15 +2,12 @@
 
     require "validaciones.php";
 
-    // Campos del formulario que se asignan a variables
     $id = $_POST['id'];
     $nombre = $_POST['nombre'];
     $apellido = $_POST['apellido'];
     $telefono = $_POST['telefono'];
     $direccion = $_POST['direccion'];
 
-
-    // Verificar si todas las validaciones pasan
     if (
         validarId($id) == true &&
         validarNombre($nombre) == true &&
@@ -19,7 +16,6 @@
         validarDireccion($direccion) == true
     )
     {
-        // Si las validaciones pasan, se muestran los campos recibidos
         echo "ID: $id  <br />";
         echo "Nombre: $nombre  <br />";
         echo "Apellido: $apellido  <br />";
@@ -29,5 +25,3 @@
     }
     else 
         echo "Fallo la validacion de los campos";
-    
-
--- validaciones.php
+++ validaciones.php
@@ -1,28 +1,24 @@
 <?php 
 
     function validarId($id){
-        // Validar que el ID recibido sea numerico
         if (isset($id) && 
             is_numeric($id)) 
             return true;
     }
 
     function validarNombre($nombre){
-        // Validar que el nombre recibido tenga menos de 20 caracteres
         if (isset($nombre) && 
             strlen($nombre)<20) 
             return true;
     }
 
     function validarApellido($apellido){
-        // Validar que el apellido recibido tenga menos de 20 caracteres
         if (isset($apellido) && 
             strlen($apellido)<20) 
             return true;
     }
 
     function validarTelefono($telefono){
-        // Validar que el telefono recibido sea de 9 cifras
         if (isset($telefono) && 
             is_numeric($telefono) && 
             strlen((string) $telefono)==9) 
@@ -30,7 +26,6 @@
     }
 
     function validarDireccion($direccion){
-        // Validar que la direccion recibida tenga menos de 50 caracteres
         if (isset($direccion) &&
             strlen($direccion)<50)
             return true;
