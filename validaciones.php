c25d0f3ca224c9dcc5ad55e618a28523b9279e5c
Sebastian Leguisamo <sleguisamoberri@gmail.com>
2022-05-16 21:14:41 -0300

    se agrego dos funciones nombre y apellido

--- validaciones.php
+++ validaciones.php
@@ -6,45 +6,36 @@
 
         };
 
-        // Validar que el ID recibido sea numerico
+        
     }
 
     function validarNombre($nombre){
-      if(isset($nombre) && 
-         strlen($nombre) < 20
-         
-    )
-        return true;
-        // Validar que el nombre recibido tenga menos de 20 caracteres
+        if(isset($nombre) && 
+        strlen($nombre) < 20) {   
+   
+       return true;
+              
+       
     }
+}
 
     function validarApellido($apellido){
-
         if (isset($apellido) &&
-        strlen($apellido) < 20
+        strlen($apellido) < 20){
         
-    )
-            return true;
-        // Validar que el apellido recibido tenga menos de 20 caracteres
+  
+            return true;    
+        }
+        
     }
 
     function validarTelefono($telefono){
-        if (is_numeric($telefono)){
-            return true;
-        }
-        
-    
-  
-
-        // Validar que el telefono recibido sea de 9 cifras
+                 
+              // Validar que el telefono recibido sea de 9 cifras
     }
 
     function validarDireccion($direccion){
-        if (isset($direccion) &&
-        strlen($direccion) < 50
-       
-    )
-            return true;
+                 
         // Validar que la direccion recibida tenga menos de 50 caracteres
     }
 
